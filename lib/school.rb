0adeff24d9375cd0514a0705914721d9d1e8381d
github username <github email address>
2019-06-25 18:29:10 -0400

    school domain done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+
+        end
+    end
+
+    def grade(grade)
+        selected_students = @roster.select do |roster_grade|
+            roster_grade == grade
+        end
+        selected_students[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students_in_grade|
+            students_in_grade.sort!
+        end
+    end
+
+end
